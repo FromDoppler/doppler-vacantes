432ffdd6a34fd00eccb60f23637f0677e111c0a6
MS\hcardoso <hernan.f.cardoso@gmail.com>
2021-08-04 16:54:07 -0300

    esto es una prueba

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Making Sense{" "}
           <a href="https://makingsense.hiringroom.com/jobs">
-            Oportunidades de empleo!
+            Oportunidades de empleo wiiiiiiiiiiiiiiiii!
           </a>
         </h1>
 
