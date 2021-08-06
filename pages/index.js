d380277faf1068c92a2b7bceec95d0f1232734a4
ornella-688 <ornella.cicalello@gmail.com>
2021-08-06 10:36:49 -0300

    Commit test

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Making Sense{" "}
           <a href="https://makingsense.hiringroom.com/jobs">
-            Oportunidades de empleo Hernan Pereira v2!
+            Oportunidades de empleo Orne Cicalello v2!
           </a>
         </h1>
 
