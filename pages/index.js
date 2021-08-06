5b84d59a606be683b0b2bc037861227d0c491bed
MS\hcardoso <hernan.f.cardoso@gmail.com>
2021-08-06 12:31:30 -0300

    v9

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Making Sense{" "}
           <a href="https://makingsense.hiringroom.com/jobs">
-            Oportunidades de empleo Hernan cardoso v5!
+            Oportunidades de empleo Hernan cardoso v8!
           </a>
         </h1>
 
